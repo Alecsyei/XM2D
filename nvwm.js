289a69272252e72e696e05f99f8118c98577cb9f
алэксеи <141669988+Alecsyei@users.noreply.github.com>
2023-08-11 21:57:28 +0200

    Update nvwm.js

--- nvwm.js
+++ nvwm.js
@@ -9,35 +9,44 @@
     const arrow = document.createElement("img");
     arrow.src = "images/arrowdown.svg";
 
-    //import digitText_1 from 'nvwm.json' assert {type: 'json'};
-    //const digitText = JSON.parse(digitText_1);
-
-    const levels = { "Level4": {
+    const levels = { 
+    "level4": {
       "itemtest": 6,
       "item1": 472,
       "item2": 263,
       "item3": 7652,
       "item4": 4781,
       "item5": 84724,
-      "item6": 74357
-    }, "level5": {
+      "item6": 74357}, 
+    "level5": {
       "item1": 61458,
       "item2": 837542,
       "item3": 675247,
       "item4": 235263,
       "item5": 672581,
-      "item6": 126428
-    }, "level6": {
+      "item6": 126428}, 
+    "level6": {
       "item1": 3658478,
       "item2": 2418326,
       "item3": 3846826,
       "item4": 4712842,
       "item5": 62353182,
-      "item6": 35846764
-    }};
+      "item6": 35846764}};
 
     function level4(){ //level4 function
-      arrowParent = document.getElementById(levels.Level4.itemtest);
+      let currentItemNumber = 1;
+      let currentItemName = "item" + currentItemNumber;
+//
+    var number = levels.level4.[currentItemName],
+    output = [],
+    sNumber = number.toString();
+
+    for (var i = 0, len = sNumber.length; i < len; i += 1) {
+        output.push(+sNumber.charAt(i))
+    }
+//
+        
+      arrowParent = document.getElementById(levels.level4.[output.1]);
       arrowParent.appendChild(arrow);
 
       
